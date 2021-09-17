4daa0f8b7c64e3d5e9f18736072baa283540782e
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2021-09-17 16:17:40 +0300

    migrated to null safety

--- lib/key_listener_plugin.dart
+++ lib/key_listener_plugin.dart
@@ -6,20 +6,20 @@ class KeyListenerPlugin {
   static const MethodChannel _channel =
       const MethodChannel('key_listener_plugin');
 
-  static Stream<int> _keyStream;
+  static Stream<int?>? _keyStream;
 
-  static Future<bool> checkAvailabilityPermission() async {
+  static Future<bool?> checkAvailabilityPermission() async {
     print('STARTING HERE');
-    final bool hasPermission =
+    final bool? hasPermission =
         await _channel.invokeMethod('checkAvailabilityPermission');
     return hasPermission;
   }
 
-  static Stream<int> get keyStream {
+  static Stream<int?>? get keyStream {
     EventChannel _eventChannel = const EventChannel('keyStream');
     if (_keyStream == null) {
       _keyStream =
-          _eventChannel.receiveBroadcastStream().map<int>((event) => event);
+          _eventChannel.receiveBroadcastStream().map<int?>((event) => event);
     }
 
     return _keyStream;
